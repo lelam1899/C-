bbf23d3a584a30c55aced2dc22e36abd7887fbdd
lelam1899 <45811360+lelam1899@users.noreply.github.com>
2019-05-07 08:09:21 +0700

    Lab_Assignment2

---
+++ Lab-Assignment2/Product.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lab_Assignment2
+{
+    class Product
+    {
+        string name;
+        int id;
+        float price;
+        int qty;
+        string image;
+        string desc;
+        List<string> gallery = new List<string>();
+
+
+        public Product()
+        {
+
+        }
+
+        public Product(int id, string name, float price, int qty, string image, string desc, List<string> list)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.qty = qty;
+            this.image = image;
+            this.desc = desc;
+            for (int i = 0; i < list.Count && i < 10; i++)
+            {
+                gallery.Add(list[i]);
+            }
+
+        }
+        public void getInfo()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Price: " + price);
+            Console.WriteLine("Quanty: " + qty);
+            Console.WriteLine("Image: " + image);
+            Console.WriteLine("Description: " + desc);
+        }
+        public void GetQtyInfo()
+        {
+            if (this.qty > 0)
+            {
+                Console.WriteLine("In Stock: " + qty);
+            }
+            else
+            {
+                Console.WriteLine("Out of stock");
+            }
+        }
+        public void AddToGallery(string img)
+        {
+            if (gallery.Count < 10)
+            {
+                gallery.Add(img);
+            }
+            else
+            {
+                Console.WriteLine("Gallery is full. ");
+            }
+        }
+        public void RemoveFromGallery()
+        {
+            Console.WriteLine("Remove at? ");
+            int i = Int16.Parse(Console.ReadLine());
+            gallery.RemoveAt(i);
+        }
+
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("item1");
+            Product product = new Product(1, "sp1", (float)5.5, 5, "image1", "hot", list);
+            product.getInfo();
+        }
+    }
+}
